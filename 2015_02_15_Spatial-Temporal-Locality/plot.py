f914af3ff03d29534a4f14455c823087415bad74
Chris Sullivan <sullivan@nscl.msu.edu>
2016-02-15 23:32:55 -0500

    Added plot of models

--- 2015_02_15_Spatial-Temporal-Locality/plot.py
+++ 2015_02_15_Spatial-Temporal-Locality/plot.py
@@ -5,7 +5,9 @@ from setfont import *
 def plot_optimization(ax,data):
     opt = [0,1,2,3]
     for i in range (0,8)[1::2]:
-        ax.plot(opt,data[:,i,1])
+        ax.plot(opt,data[:,i,1],color='black')        
+    for i in range (0,8)[0::2]:
+        ax.plot(opt,data[:,i,1],color='red')
 
     ax.set_yscale('log')
     setticks(ax,xlog=False,xminor=0.5,xmajor=1.0,ylog=True)
@@ -18,11 +20,12 @@ def plot_block_v_unblocked(ax,data):
         ax.plot(data[i,0::2,0],data[i,0::2,1],color='red')   # unblocked
         ax.plot(data[i,1::2,0],data[i,1::2,1],color='black') # blocked
         print data[i,0::2,1]
+    ax.plot(data[0,0::2,0],data[0,0::2,2],color='blue') # model 1
+    ax.plot(data[0,0::2,0],data[0,0::2,3],color='green') # model 2
     ax.set_yscale('log')
     ax.set_xscale('log')
     setticks(ax,xlog=True,ylog=True)
     pylab.show()
-        
     
     
 if __name__ =="__main__":
@@ -50,5 +53,5 @@ if __name__ =="__main__":
     data = np.asarray(optclass)     #data[optimization,row,col]
 
 
-    plot_optimization(ax,data)
-    #plot_block_v_unblocked(ax,data)
+    #plot_optimization(ax,data)
+    plot_block_v_unblocked(ax,data)
