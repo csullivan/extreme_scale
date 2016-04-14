198485bd8bcb7f1a32a8f2f9142d4cbc274d2f85
Chris Sullivan <sullivan@nscl.msu.edu>
2016-04-13 22:38:46 -0400

    Added least squares fit to plotting routines

--- 2016_04_08_MPI-Basics/plot.py
+++ 2016_04_08_MPI-Basics/plot.py
@@ -2,6 +2,8 @@ import pylab
 import numpy as np
 from setfont import *
 import pdb
+from scipy.optimize import curve_fit
+
 
 def plot_size_v_time(ax,data):
 
@@ -21,6 +23,52 @@ def plot_size_v_time(ax,data):
     line, = ax.plot(data[2][0],data[2][3],label='headtohead')
     line.set_dashes([2, 2])
 
+    ax.set_ylim(1e-4,1e0)
+    ax.set_yscale('log')
+    ax.set_xscale('log')
+    setticks(ax,xlog=True,ylog=True)
+    ax.legend(loc='upper left', prop={'size':9})
+
+def plot_size_v_time_fit(ax,data):
+
+
+
+    postal_time = lambda n,s,r: s+r*n
+    # popt,pcov = curve_fit(postal_time,data[0][0],data[0][1])
+    # fit = lambda n: postal_time(n,popt[0],popt[1])
+    # ax.plot(data[0][0],[fit(x) for x in data[0][0]],label='s+rn')
+    x = []
+    y = []
+    for i,n in enumerate(data[0][0]):
+        if n < 8193 and n > 4:
+            x.append(n)
+            y.append(data[0][1][i])
+    x = np.asarray(x)
+    y = np.asarray(y)
+
+    ax.plot(x,y,label='blocking',color='black')
+    popt,pcov = curve_fit(postal_time,x,y)
+    fit = lambda n: postal_time(n,popt[0],popt[1])
+    print popt
+    ax.plot(x,[fit(xi) for xi in x],label='s1+r1*n',color='red')
+
+    x = []
+    y = []
+    for i,n in enumerate(data[0][0]):
+        if n > 8000:
+            x.append(n)
+            y.append(data[0][1][i])
+    x = np.asarray(x)
+    y = np.asarray(y)
+
+    ax.plot(x,y,label='blocking',color='black')
+    popt,pcov = curve_fit(postal_time,x,y)
+    fit = lambda n: postal_time(n,popt[0],popt[1])
+    print popt
+    ax.plot(x,[fit(xi) for xi in x],label='s2+r2*n',color='blue')
+
+
+    ax.set_ylim(1e-4,1e0)
     ax.set_yscale('log')
     ax.set_xscale('log')
     setticks(ax,xlog=True,ylog=True)
@@ -38,7 +86,7 @@ if __name__ =="__main__":
     headtohead = []
     messagesize = []
     count = 0
-    for i,line in enumerate(open("./output.dat")):
+    for i,line in enumerate(open("./bluewaters.dat")):
         if len(line.split()) == 0:
             continue
         if '####' in line:
@@ -69,8 +117,8 @@ if __name__ =="__main__":
     headtohead = np.asarray(headtohead)
     messagesize = np.asarray(messagesize)
     data.append([messagesize, blocking, nonblocking, headtohead])
-    #data = np.asarray(data)
 
-    plot_size_v_time(ax,data)
-    pylab.savefig("./output.pdf")
+
+    plot_size_v_time_fit(ax,data)
+    pylab.savefig("./bluewaters_fit.pdf")
     #pylab.show()
